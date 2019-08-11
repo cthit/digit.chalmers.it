abdde8d0a04184e7a70026bf7fce5ddd1c267b20
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2019-08-11 20:12:02 +0200

    Add members page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Switch} from 'react-router-dom';
 import {Route} from 'react-router';
 
 import Home from 'page/Home.js';
+import Members from 'page/Members.js';
 import NotFound from 'page/NotFound.js';
 
 // App routes the user to the correct page.
@@ -11,6 +12,7 @@ export default function App() {
         <BrowserRouter className="App">
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/members" component={Members} />
             <Route path="/" component={NotFound} />
           </Switch>
         </BrowserRouter>
